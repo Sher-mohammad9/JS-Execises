d3464e8375754a81999ea33fc27250c91e7204e2
Sher-mohammad9 <hassan.deshwali0@gmail.com>
2023-02-13 14:12:30 +0530

    Add new qeustions

--- Questoins.js
+++ Questoins.js
@@ -1,179 +1,346 @@
-// 1. let str = "My name is Wecode Academy" string ko space se split krke length btani hai.
+// // 1. let str = "My name is Wecode Academy" string ko space se split krke length btani hai.
 
-let strSplit = "My name is Wecode Academy";
-let strToArr = strSplit.split(" ");
-console.log(strToArr.length);
+// let strSplit = "My name is Wecode Academy";
+// let strToArr = strSplit.split(" ");
+// console.log(strToArr.length);
 
-// 2. String palindrome check ababa, helleh ?
+// // 2. String palindrome check ababa, helleh ?
 
-let strPalnd = "str madam rts";
-let checkPalnd = "";
-for(let i = 1; i <= strPalnd.length; i++){
-   checkPalnd += strPalnd.at(-i);
+//    let strP = "str madam rts";
+//    let checkP = "Is Palindrome";
+//    for(let i = 0; i < strP.length; i++){
+//       if(strP[i].toLowerCase() === strP.at(-(i+1))){
+//             continue;
+//       }else{
+//             checkP = "Not Palindrome";
+//             break;
+//       }
+//    }
+//    console.log(checkP);
+
+
+
+// // 3. 9581894461
+// // 95818***
+// // ***94461
+// // 958*944*
+
+// let str4 = "9581894461";
+// let sub1 = str4.substring(0, 3);
+// let pad1 = sub1.padEnd(5, "*");
+// let sub2 = str4.substring(5,8);
+// let pad2 = sub2.padEnd(5,"*")
+// let con = pad1.concat(pad2);
+// console.log(con)
+
+// // 4. Reverse String 
+// // Hello -> olleH
+// // Bye -> eyB
+
+// let stri = "Hello";
+// let strRev = "";
+// for(let i=1; i<=stri.length; i++){
+//    strRev += stri.at(-i);
+// }
+// console.log(strRev);
+
+
+// // 5. Captialize first character 
+// // wecode -> Wecode 
+// // hello -> Hello
+
+// let str = "wecode"
+// let strArr = str.split(" ")
+// let strCap = "";
+// for(let val of strArr){
+//      strCap += val.charAt(0).toUpperCase().concat(val.slice(1))
+//   }
+// console.log(strCap);
+
+
+// // 6. String
+// // uppercase ->  lowercase 
+// // lowercase -> uppercase
+// // HeLlo -> hElLO
+// // WECodE -> wecODe
+
+// let strSmallCap1 = "HeLlo";
+// let strSmallCap2 = "";
+// for(let val of strSmallCap1){
+//    strSmallCap2 += val === val.toUpperCase() ? val.toLowerCase() : val.toUpperCase();
+// }
+// console.log(strSmallCap2)
+
+// // 7. Camelize
+// // Hello World -> helloWorld
+// // my name is Sajid -> myNameIsSajid
+// // Wecode Academy -> wecodeAcademy
+
+// let strSpace = "my name is Sajid";
+// let removeSpace = "";
+// for(let val of strSpace){
+//    if(val ===  " "){
+//       continue;
+//    }else{
+//       removeSpace += val;
+//    }
+// }
+// console.log(removeSpace)
+
+
+// //8. String me kitne space hai vo count krne hain.
+
+// let str1 = "my name is Sajid";
+// let count = 0;
+// for(let val of str1){
+//    if(val ===  " "){
+//       count++;
+//    }
+// }
+// console.log(count)
+
+
+// // 9. Using repeat function 
+// // 1111111111
+// // 222222222
+// // 33333333
+// // 4444444
+// // 555555
+// // 66666
+// // 7777
+// // 888
+// // 99
+// // 0
+
+// let print = 1;
+// for(let i=10; i>=1; i--){
+//    console.log(String(print).repeat(i))
+//    if(i === 2){
+//       print = 0
+//       continue;
+//    }
+//    print++;
+// }
+
+// // 10. "My name is Sajid Khan";
+// // input me ek word lena hai "is"
+// // ans: Sajid Khan
+
+// let userName = "My name is Sajid Khan";
+// let splitName = prompt("Enter 'is'")
+// let s = userName.split("My name " + splitName);
+// console.log(s.join(""))
+
+
+// // 14. Hello My name is Sajid KHan 
+// // input me ek word lena hai aur vo word string me hai to uski starting
+// // index btani hai. String and input dono prompt se lene hain 
+
+// let userName1 = prompt("Enter a single line statement");
+// let checkStr = prompt("Single line statement includes this word or not");
+// let strIndex = userName1.indexOf(checkStr)
+// strIndex > 0 ? console.log(`Single line statement includes this word '${checkStr}' in ${strIndex} index`) : console.log("Not includes")
+
+
+// // 15. ek string leni hai. agar usme string me $, _, number, a-z, A_Z 
+// // ke alawa koi aur character hai to use remove krna hai. Regex use ni krni hai 
+// // We%^%$Cod!@#e -> We$Code
+
+// let strin = "We%^%$Cod!@#e";
+// let string = "";
+// let alphabatL = "abcdefghijklmnopqrstuvwxyz"
+// for(let val of strin){
+//     if(val === "$" || val === "_" || alphabatL.includes(val) || alphabatL.toUpperCase().includes(val)){
+//            string += val;
+//     }
+// }
+// console.log(string)
+
+
+//16. add, minus, mul, divide ye 4 functions bnane hai using arrow function and also without return 
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(10,10))
+
+const minus = (num1, num2) => num1 - num2;
+console.log(minus(10,10))
+
+const mul = (num1, num2) => num1 * num2;
+console.log(mul(10,10))
+
+const divide = (num1, num2) => num1 / num2;
+console.log(divide(10,10))
+
+
+//17. calculator, function expression me bnana hai. 
+// Prompt me tume operation puchna hai. operation jaise add, divide, +, -, plus, 
+// minus fir usse 2 numbers puchne hain aur operation kran hai
+
+const calculator = function (){
+   let opertor = prompt("What operation do you want to do with two number +, -, *, /");
+   let num1 = +prompt("Enter first number");
+   let num2 = +prompt("Enter second number");
+   if(opertor === "+"){
+      console.log(num1 + num2)
+   }else if(opertor === "-"){
+      console.log(Math.abs(num1 - num2))
+   }else if(opertor === "*"){
+      console.log(num1 * num2)
+   }else if(opertor === "/"){
+      console.log(num1 / num2)
+   }
 }
-strPalnd === checkPalnd ?  console.log("String is palindrome") : console.log("String is not palindrome")
+calculator();
 
 
-// 3. 9581894461
-// 95818***
-// ***94461
-// 958*944*
+//18. function slice, ek string pass krni hai. 
+// ek starting indexedDB, 2nd end index, aur fir vo slice krke vo string return krni hai.; 
+// agar index galat di hai to error print krni hai using console.error; 
 
-let str4 = "9581894461";
-let sub1 = str4.substring(0, 3);
-let pad1 = sub1.padEnd(5, "*");
-let sub2 = str4.substring(5,8);
-let pad2 = sub2.padEnd(5,"*")
-let con = pad1.concat(pad2);
-console.log(con)
-
-// 4. Reverse String 
-// Hello -> olleH
-// Bye -> eyB
-
-let stri = "Hello";
-let strRev = "";
-for(let i=1; i<=stri.length; i++){
-   strRev += stri.at(-i);
-}
-console.log(strRev);
-
-
-// 5. Captialize first character 
-// wecode -> Wecode 
-// hello -> Hello
-
-let str = "wecode"
-let strArr = str.split(" ")
-let strCap = "";
-for(let val of strArr){
-     strCap += val.charAt(0).toUpperCase().concat(val.slice(1))
-  }
-console.log(strCap);
-
-
-// 6. String
-// uppercase ->  lowercase 
-// lowercase -> uppercase
-// HeLlo -> hElLO
-// WECodE -> wecODe
-
-let strSmallCap1 = "HeLlo";
-let strSmallCap2 = "";
-for(let val of strSmallCap1){
-   strSmallCap2 += val === val.toUpperCase() ? val.toLowerCase() : val.toUpperCase();
-}
-console.log(strSmallCap2)
-
-// 7. Camelize
-// Hello World -> helloWorld
-// my name is Sajid -> myNameIsSajid
-// Wecode Academy -> wecodeAcademy
-
-let strSpace = "my name is Sajid";
-let removeSpace = "";
-for(let val of strSpace){
-   if(val ===  " "){
-      continue;
+function slice(str, startIn, endIn){
+   if(startIn >= 0 && str.length >= endIn){
+      return str.substring(startIn, endIn);
    }else{
-      removeSpace += val;
+      console.error("Incorrect index")
    }
 }
-console.log(removeSpace)
+console.log(slice("Wecode Academy", 0, 5))
 
 
-//8. String me kitne space hai vo count krne hain.
+//19. function repeat, first argument ek string pass krni hai. 2nd argument me number pass krna hai.
+// di gyi string ko 1 se leke number tak repeat krvana hai using loop. 
+// repeat function ka use ni krna hai. khud ka repeat function bnana hai 
 
-let str1 = "my name is Sajid";
-let count = 0;
-for(let val of str1){
-   if(val ===  " "){
-      count++;
+const repeat = function (str, num){
+      for(let i=1; i <= num; i++){
+         let str1 = "";
+         for(let j=1; j<=i; j++){
+            str1 += str
+         }
+         console.log(str1)
+      }
    }
-}
-console.log(count)
+repeat("Hello ", 5)
 
+//20. function concat, 5 string deni hai arugments me aur un 5 string ko concat krke return krna hai using arrow function 
 
-// 9. Using repeat function 
-// 1111111111
-// 222222222
-// 33333333
-// 4444444
-// 555555
-// 66666
-// 7777
-// 888
-// 99
-// 0
-
-let print = 1;
-for(let i=10; i>=1; i--){
-   console.log(String(print).repeat(i))
-   if(i === 2){
-      print = 0
-      continue;
+const concat = (...str) => {
+   let conStr = '';
+   for(let val of str){
+      conStr += val
    }
-   print++;
+   return conStr;
 }
+console.log(concat("Hello ","my ","name ","is ","Sher Mohammad"));
 
-// 10. "My name is Sajid Khan";
-// input me ek word lena hai "is"
-// ans: Sajid Khan
+//21. padStart and padEnd function ko khudse bnana hai without using string function. 
+// ***Wecode
+// padding(false, true, "Sajid", "**", 5);
+// Wecode****
 
-let userName = "My name is Sajid Khan";
-let splitName = prompt("Enter 'is'")
-let s = userName.split("My name " + splitName);
-console.log(s.join(""))
+function padding(start, end, str, sym, num){
+   let strSym = "";
+   if(start){   
+   for(let i=1; i<=num; i++){
+       strSym += sym;
+   }
+   return strSym.concat(str);
+  }else if(end){
+   for(let i=1; i<=num; i++){
+       strSym += sym;
+   }
+   return str.concat(strSym);   
+  }
+}
+console.log(padding(false, true, "Wecode", "*", 5));
 
 
-// 14. Hello My name is Sajid KHan 
-// input me ek word lena hai aur vo word string me hai to uski starting
-// index btani hai. String and input dono prompt se lene hain 
+//22. printPyramid("wecode", 5, true);
+// wecodewecodewecodewecodewecode
+// wecodewecodewecodewecode
+// wecodewecodewecode
+// wecodewecode
+// wecode
 
-let userName1 = prompt("Enter a single line statement");
-let checkStr = prompt("Single line statement includes this word or not");
-let strIndex = userName1.indexOf(checkStr)
-strIndex > 0 ? console.log(`Single line statement includes this word '${checkStr}' in ${strIndex} index`) : console.log("Not includes")
-
-
-// 15. ek string leni hai. agar usme string me $, _, number, a-z, A_Z 
-// ke alawa koi aur character hai to use remove krna hai. Regex use ni krni hai 
-// We%^%$Cod!@#e -> We$Code
-
-let strin = "We%^%$Cod!@#e";
-let string = "";
-let alphabatSmall = "abcdefghijklmnopqrstuvwxyz"
-let alphabatCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for(let val of strin){
-    if(val === "$" || val === "_" || alphabatSmall.includes(val) || alphabatCap.includes(val)){
-           string += val;
+const printPattern = function (str, num, rev) {
+    if(num > 0 && rev){
+       for(let i = num; i >= 1; i--){
+         console.log(str.repeat(i))
+       }
+    }else if(num > 0 && !rev){
+       for(let i = 1; i <= num; i++){
+       console.log(str.repeat(i))      
     }
 }
-console.log(string)
+}
+printPattern("wecode", 5, true);
 
 
+//23. user se name puchna hai. then user ka mobile number puchan hai, then user se birthyear puchni hai. 
+// agar user ki age 18+ ho ri hai to usse 3 question puchne hai. Agar vo 3 question ke answer shi deta hia 
+// to Congratulations otherwise Sorry you  have lost the game show krnaa hai using alert box 
+// age user ki age 18 se kam hai to message show kro "You are not allowed to play the game."
+
+function playGame(){
+   const userName = prompt("Enter your name");
+   const mobileNumber = +prompt("Enter your mobile number");
+   const dateOfBirth = +prompt("Enter your birth year");
+   const currentYear = new Date().getFullYear();
+   const userAge = Math.abs(dateOfBirth - currentYear)
+   if(userAge >= 18){
+       let flag = false;
+       let Q1 = prompt("What is the capital of india");
+       if(Q1.toLowerCase() === "delhi"){
+           flag = true;
+           Q1 = prompt("What is the capital of rajashthan");
+       }
+       if(Q1.toLowerCase() === "jaipur" && flag){
+           flag = true;
+           Q1 = +prompt("How much 2 * 2");
+       }
+       if(Q1 === 4 && flag){
+          console.log(`Congratulation ${userName} for win the game`)
+       }else{
+          console.log(`Sorry ${userName} you  have lost the game`)
+       }
+   }else{
+          console.log("You are not allowed to play the game.")
+   }
+}
+playGame()
+
+//24.Check even odd number. 
+
+function checkEvenOdd(num1, num2){
+   if (num1 % 2 !== 0 && num1 < 100) {
+          return "Yes";
+   }else if(num2 % 2 === 0 && num2 > 400){
+      return "No"
+   }else{
+      return "Hello"
+   }
+}
+console.log(checkEvenOdd(919,400))
+
+//25.Print pyramind pattern.
+
+for(let i = 1; i<=5; i++){
+   let space = "";
+   for(let k=1; k <= 5-i; k++){
+       space += " ";
+   }
+   for(let j = 1; j <= i; j++){
+     space += "* "
+   }
+   console.log(space);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-// .  Trigno, Apti, Math, Quant  Mathmetics  English, Tense, Active, Passive  English true JavaScript false Java 
+// 26. Trigno, Apti, Math, Quant  Mathmetics  English, Tense, Active, Passive  English true JavaScript false Java 
 // Turkey, India, Japan  Country Rajasthan  Biggest State  Delhi Capital 18 Adult 11 Minor default case Hello JavaScript
 
-const input = "India";
-switch(input.toLowerCase()){
+const input = "india";
+switch(input){
 case "trigno":
 case "apti":
 case "math":
@@ -222,7 +389,7 @@ default:
 
 }
 
-//37. 1 to 50 print krvana hai Number odd hai to continue krna hai Even number and 40 < hai to print number krdo 
+//27. 1 to 50 print krvana hai Number odd hai to continue krna hai Even number and 40 < hai to print number krdo 
 // otherwise loop ko break krdo
 
 for(let i=1; i <= 50; i++){
@@ -235,18 +402,35 @@ for(let i=1; i <= 50; i++){
    // console.log(i)
 }
 
-//38. Write a program to print Fibonacci series of n terms where n is input by user :
+//28. Write a program to print Fibonacci series of n terms where n is input by user :
 
 let a = 1;
 let b = 1;
 let c = 0;
 for(let i=1; i<=200; i++){
-// console.log(c)
+console.log(c)
 a = b;
 b = c;
 c = a+b;
-if(c > 200){
-	break;
+if(c >= 21){
+	c += 3;
 }
 }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
